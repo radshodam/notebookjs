4283a377d4e2bc8cfe89589d3656f38a5631331b
mohamadZareai <mohamadzareai@gmail.com>
2020-12-06 12:05:28 +0330

    creat note book

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,175 @@
-// variables
-const noteList = document.querySelector('#note-list')
+// }
+// // variables
+// const noteList = document.querySelector('#note-list')
 
 
-// eventlisteners
-eventlisteners()
-    // form submission
+// // eventlisteners
+// eventlisteners()
+
+// function eventlisteners() {
+//     // form submission
+//     document.querySelector('#form').addEventListener('submit', newNote)
+
+//     // remove note
+//     document.querySelector('#note-list').addEventListener('click', removeNote)
+
+//     // get data from localstorage on loaded
+//     document.addEventListener('DOMContentLoaded', localStorageOnload)
+// }
+
+
+
+// // functions
+
+// // Adding new note to the list
+// function newNote(e) {
+//     e.preventDefault()
+//         // access to the value
+//     const note = document.querySelector('#note').value
+
+//     // create remove element
+//     const removeBtn = document.createElement('a')
+//     removeBtn.textContent = 'X'
+//     removeBtn.classList = 'remove-note'
+
+//     // create <li> tag
+//     const li = document.createElement('li')
+//     li.appendChild(document.createTextNode(note))
+
+//     // adding remove btn to the li
+//     li.appendChild(removeBtn)
+
+//     // adding li to the note-list
+//     noteList.appendChild(li)
+
+//     this.reset()
+
+//     addNoteToLocalStorage(note)
+
+//     alert('یادداشت با موفقیت ذخیره شد!')
+// }
+
+// // remove Note from list
+// function removeNote(e) {
+//     if (e.target.classList.contains('remove-note')) {
+//         e.target.parentElement.remove()
+//     }
+
+//     // aslo remote the note from the Local Storage
+//     removeNoteLocalStorage(e.target.parentElement.textContent)
+// }
+
+// // adding note to the local storage
+// function addNoteToLocalStorage(note) {
+//     // get the notes from localStorage 
+//     const notes = getNotesFromLocalStorage()
+
+//     // add new note to the notes array
+//     notes.push(note)
+
+//     // add new notes Array to the localStorage
+//     localStorage.setItem('notes', JSON.stringify(notes))
+// }
+
+// // get notes from localStorage
+// function getNotesFromLocalStorage() {
+//     let notes;
+
+//     // get previous notes from localStorage
+//     let getFromLS = localStorage.getItem('notes');
+//     if (getFromLS === null) {
+//         // if not exist create empty array
+//         notes = []
+//     } else {
+//         // if exist convert to the array 
+//         notes = JSON.parse(getFromLS)
+//     }
+
+//     return notes
+// }
+
+// // get data from local storage on load
+// function localStorageOnload() {
+//     const notes = getNotesFromLocalStorage();
+
+//     // print each item of array
+//     notes.forEach(function(note) {
+//         // create remove element
+//         const removeBtn = document.createElement('a')
+//         removeBtn.textContent = 'X'
+//         removeBtn.classList = 'remove-note'
+
+//         // create <li> tag
+//         const li = document.createElement('li')
+//         li.appendChild(document.createTextNode(note))
+
+//         // adding remove btn to the li
+//         li.appendChild(removeBtn)
+
+//         // adding li to the note-list
+//         noteList.appendChild(li)
+//     });
+// }
+
+// // also Remove note from localStorage
+// function removeNoteLocalStorage(noteContent) {
+//     // delete X from the contetn
+//     const noteDelete = noteContent.substring(0, noteContent.length - 1)
+
+//     // get notes from localstorage
+//     const notesFromLS = getNotesFromLocalStorage()
+
+//     notesFromLS.forEach(function(note, index) {
+//         if (note === noteDelete) {
+//             notesFromLS.splice(index, 1)
+//         }
+//     });
+
+//     // set new array of notes to the local storage
+//     localStorage.setItem('notes', JSON.stringify(notesFromLS))
+
+// }
+
+//variables
+const forms = document.querySelector("#form");
+// console.log(forms)
+const noteList = document.querySelector("#note-list");
+// console.log(noteList);
+
+//eventlisteners
+eventlisteners();
 function eventlisteners() {
-    document.querySelector('#form').addEventListener('submit', newNote)
+  forms.addEventListener("submit", newnote);
+  noteList.addEventListener("click" ,removenote)
 }
 
+// function
 
-// functions
+function newnote(e) {
+  e.preventDefault();
+  // console.log(document.querySelector("#note").value);
+ 
+  const note = document.querySelector("#note").value;
+ 
 
-// Adding new note to the list
-function newNote(e) {
-    e.preventDefault()
-        // access to the value
-    const note = document.querySelector('#note').value
-    if (note == "") {
-        console.log("empty");
-    } else {
+if (note==="") {
+} else {
+    const li = document.createElement("li");
+    const btnremove=document.createElement("a")
+    btnremove.textContent="X"
+    btnremove.classList="remove-note"
+    li.appendChild(btnremove)
+    li.appendChild(document.createTextNode(note))
+    noteList.appendChild(li) 
+}
+  console.log(btnremove)
+}
 
-
-        // create remove element
-        const removeBtn = document.createElement('a')
-        removeBtn.textContent = 'X'
-        removeBtn.classList = 'remove-note'
-
-
-        console.log(removeBtn)
-            // create <li> tag
-        const li = document.createElement('li')
-        li.appendChild(document.createTextNode(note))
-
-        // adding remove btn to the li
-        li.appendChild(removeBtn)
-
-        // adding li to the note-list
-        noteList.appendChild(li)
+function removenote(e){
+    // console.log(e.target);
+    // console.log(e.target.classList.contains('remove-note'));
+    if (e.target.classList.contains('remove-note')) {
+       const removeli= e.target.parentElement
+       removeli.remove()
     }
 }
